d3b727cdcad5f58798c1563c00d4fd0e9d2daee8
Makarand Jadhav <lxgtx1080@gmail.com>
2025-09-11 20:11:56 +0530

    Initial Commit

---
+++ day2.py
@@ -0,0 +1,8 @@
+name1 = "Swaraj"
+name2 = "Krupesh"
+name3 = "Rushikesh"
+
+print(name1,name2,name3)
+
+age = 20
+
---
+++ day3.py
@@ -0,0 +1,52 @@
+'''
+# Arithmatic Operator 
+# 
+a = 5 
+b = 10
+c = a + b
+print("Result is:",c)
+
+# Substraction 
+a = 50
+b = 45
+c = a - b
+print("Substraction Result is:",c)
+'''
+#------------------------------------------------
+
+#a = input("Value A is:")
+#b = input("Value B is:")
+#c = a + b
+#print("Result is:",c)
+#------------------------------------------------
+
+
+# Comparison Operators
+'''x = 7
+y = 6
+print(x == y) # False (equal to equal to)
+print(x != y) # True (not equal to)
+print(x > y) # False (greater than)
+print(x < y) # True (less than)
+print(x >= y) # False (greater than or equal to)
+print(x <= y) # True (less than or equal to)
+'''
+
+# Logical Operators
+p = 1
+q = 0
+print(p and q) # False 0
+print(p or q) # True 1
+print(not p) # False 0
+print(not q) # True 1
+
+# Assignment Operators
+num = 10
+num += 5 # num = num + 5
+print(num) # 15
+num -= 3 # num = num - 3
+print(num) # 12
+num *= 2 # num = num * 2
+print(num) # 24
+num /= 4 # num = num / 4
+print(num) # 6.0
