73e52db48f4e371b2b4afbecaf3476440fa6c452
Makarand Jadhav <lxgtx1080@gmail.com>
2025-09-15 20:12:01 +0530

    Initial Commit

---
+++ day6.py
@@ -0,0 +1,6 @@
+# Count the occurrences of "red" in the tuple
+colors = ("red", "green", "blue" , "red" , "yellow", "blue" , "red", "green", "blue")
+print(colors.count("red"))
+
+#Index of first occurrence of "blue" in the tuple
+print(colors.index("blue"))
