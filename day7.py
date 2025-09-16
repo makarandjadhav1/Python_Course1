33fc4b0c8e6a3db45ce9cdd0c676f609bd37d69a
Makarand Jadhav <lxgtx1080@gmail.com>
2025-09-16 19:59:19 +0530

    Initial Commit

--- day7.py
+++ day7.py
@@ -6,3 +6,21 @@ if age >= 18:
     print("Your are eligible to vote.")
 else:
     print("Your are not eligible to vote.")
+
+#If-elif-else Statement
+# Program to check the grade of a student based on marks
+name = input("Enter your name: ")
+marks = int(input("Enter your marks: "))
+if marks >= 90:
+    grade = 'A'
+    print("Congratulations", name, "! You have secured grade", grade)
+elif marks >= 80:
+    grade = 'B'
+    print("Congratulations", name, "! You have secured grade", grade)
+elif marks >= 70:
+    grade = 'C'
+    print("Congratulations", name, "! You have secured grade", grade)
+else:
+    grade = 'D'
+    print("Sorry", name, "! You have secured grade", grade)
+#--------------------------------
