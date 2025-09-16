416ac8ebeea527f88af5bc5dffab212ee9ddbdab
Makarand Jadhav <lxgtx1080@gmail.com>
2025-09-16 19:52:00 +0530

    Initial Commit

---
+++ day7.py
@@ -0,0 +1,8 @@
+# If-Else Statement
+# Program to check if a person is eligible to vote or not
+# Eligibility criteria: Age must be 18 or above
+age = int(input("Enter Your age: "))
+if age >= 18:
+    print("Your are eligible to vote.")
+else:
+    print("Your are not eligible to vote.")
